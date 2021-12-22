02b597e3d0f8c91cb61f2d91d1cc1ff53d62ec2e
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2021-12-22 12:16:23 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component'
+
 function App() {
   return (
-   <div>Holla!</div>
-  );
+   <div>
+     Hey dude
+     <HomePage />
+   </div>
+  )
 }
 
 export default App;
