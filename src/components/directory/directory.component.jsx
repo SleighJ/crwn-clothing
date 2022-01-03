79efc588cb61a4d1f7218731af407584502fbd4a
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2022-01-03 15:34:27 -0700

    styling boob + props

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -20,8 +20,13 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          sections.map(({ id, title }) => (
-            <MenuItem key={id} title={title} />
+          sections.map(({ id, title, imageUrl, size }) => (
+            <MenuItem 
+              key={id} 
+              title={title} 
+              imageUrl={imageUrl} 
+              size={size}
+            />
           ))
         }
       </div>
