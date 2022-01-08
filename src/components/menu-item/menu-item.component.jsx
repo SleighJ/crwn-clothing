6d3fdc67f274974874642296ac6336f82f36755c
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2022-01-07 17:09:35 -0700

    boop

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
     <div 
       className={`${size} menu-item`} 
