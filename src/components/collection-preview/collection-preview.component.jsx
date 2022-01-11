9f473441cfd1c4b016787c05e556566d31012bcd
Jonathan Jensen Jr <jonathanjensenjr@gmail.com>
2022-01-11 14:50:03 -0700

    yarn boop, boop yarn

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
+import CollectionItem from '../collection-item/collection-item.component'
+
 import './collection-preview.styles.scss'
 
 const CollectionPreview = ({ title, items }) => {
-
+  const filteredItems = items.filter((item, idx) => idx < 4)
+  
   return (
-    <div>
+    <div className='collection-preview'>
       <h1>{title.toUpperCase()}</h1>
       <div className='preview'>
         {
-          items.map(({ id, name }) => {
-            return <div key={id}>{name}</div>
+          filteredItems.map(({ id, name, ...otherItemProps }) => {
+            return <CollectionItem key={id} name={name} { ...otherItemProps } />
           })
         }
       </div>
